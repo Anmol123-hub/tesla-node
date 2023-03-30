9dc6baa670b9d5c73b1bcf50fa14b3ed99d520e5
Anmol Varshney <anmol.varshney@valtech.com>
2023-03-30 18:00:07 +0530

    tesla work 1

--- index.js
+++ index.js
@@ -11,20 +11,32 @@ let fs = require('fs')
 let app = express()
 const PORT = process.env.PORT || 2525
 
-// app.use(express.static(path.join(__dirname+"/public")))
+app.use(express.static(path.join(__dirname+"/tesla_angular")))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-app.use(express.static('tesla_angular'))
+
 app.use(cors())
 
 
 let Dealername = mongoose.model("Dealername", Schema({
-    dealerId:Number,
-    dealerName:String,
-    dealerEmail:String,
-    dealerPass:String,
-    dealerCity:String,
-    dealerAddress:String
+    dealerId: Number,
+    dealerName: String,
+    dealerEmail: String,
+    dealerPass: String,
+    dealerCity: String,
+    dealerAddress: String
+
+}))
+let Customerrequest = mongoose.model("Customerrequest", Schema({
+    firstname: String,
+    lastname: String,
+    contact: Number,
+    email: String,
+    pincode: Number,
+    dealerId: Number,
+    date: String,
+    time: String,
+    carModel:String
 
 }))
 const string_mongo = `mongodb+srv://${config.username}:${config.password}@cluster0.t0n6iqu.mongodb.net/${config.dbname}?retryWrites=true&w=majority`
@@ -32,8 +44,23 @@ mongoose.connect(string_mongo).then((res) => console.log("Connected"))
     .catch((err) => console.log("Error", err))
 
 app.get("/getdealerdetails", (req, res) => {
-    Dealername.find().then((db)=>res.send(db))
+    Dealername.find().then((db) => res.send(db))
 
+})
+app.post("/customerrequest", (req, res) => {
+    let a = req.body
+    let customer = new Customerrequest({
+        firstname: a.first,
+        lastname: a.last,
+        contact: a.contact,
+        email: a.email,
+        pincode: a.pincode,
+        dealerId: a.dealerId,
+        date: a.date,
+        time: a.time,
+        carModel:a.car
+    })
+    customer.save().then((updateRes) => res.send("Updated"))
 })
 
 
