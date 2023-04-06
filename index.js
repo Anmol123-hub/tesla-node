f2bf3bd67ef9db8c28e5dbd0425540609f072517
Anmol Varshney <anmol.varshney@valtech.com>
2023-04-06 15:13:55 +0530

    tesla work

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let Dealername = mongoose.model("Dealername", Schema({
 
 }))
 let Customerrequest = mongoose.model("Customerrequest", Schema({
+    custId: Number,
     firstname: String,
     lastname: String,
     contact: Number,
@@ -36,8 +37,14 @@ let Customerrequest = mongoose.model("Customerrequest", Schema({
     dealerId: Number,
     date: String,
     time: String,
-    carModel:String
+    carModel: String,
+    request: String
 
+}))
+
+let Customerid = mongoose.model("Customerid", Schema({
+    _id: String,
+    sequence_value: Number,
 }))
 const string_mongo = `mongodb+srv://${config.username}:${config.password}@cluster0.t0n6iqu.mongodb.net/${config.dbname}?retryWrites=true&w=majority`
 mongoose.connect(string_mongo).then((res) => console.log("Connected"))
@@ -45,36 +52,120 @@ mongoose.connect(string_mongo).then((res) => console.log("Connected"))
 
 app.get("/getdealerdetails", (req, res) => {
     Dealername.find().then((db) => res.send(db))
+})
+
+app.get("/getallcustomer", (req, res) => {
+    Customerrequest.find().then((db) => res.send(db))
 
 })
+
+app.get("/admin",(req,res)=>{
+    res.sendFile(__dirname + '/tesla_angular/index.html');
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(__dirname + '/tesla_angular/index.html');
+})
+app.get("/superadmin",(req,res)=>{
+    res.sendFile(__dirname + '/tesla_angular/index.html');
+})
+app.get("/drive",(req,res)=>{
+    res.sendFile(__dirname + '/tesla_angular/index.html');
+})
+
+app.post("/getcustomerrequests", (req, res) => {
+    let a = req.body
+    Customerrequest.find({ dealerId: a.id, request: "pending" }).then((db) => res.send(db))
+
+})
+app.post("/getaccepetdrequets", (req, res) => {
+    let a = req.body
+    Customerrequest.find({ dealerId: a.id, request: "accepted" }).then((db) => res.send(db))
+
+})
+app.post("/getrejectrequets", (req, res) => {
+    let a = req.body
+    Customerrequest.find({ dealerId: a.id, request: "reject" }).then((db) => res.send(db))
+
+})
+
+
 app.post("/customerrequest", (req, res) => {
     let a = req.body
-    let customer = new Customerrequest({
-        firstname: a.first,
-        lastname: a.last,
-        contact: a.contact,
-        email: a.email,
-        pincode: a.pincode,
-        dealerId: a.dealerId,
-        date: a.date,
-        time: a.time,
-        carModel:a.car
+    let id = 0
+    Customerid.findById({ _id: "custId" }).then((db) => {
+        id = db.sequence_value
+
+        let customer = new Customerrequest({
+            custId: id,
+            firstname: a.first,
+            lastname: a.last,
+            contact: a.contact,
+            email: a.email,
+            pincode: a.pincode,
+            dealerId: a.dealerId,
+            date: a.date,
+            time: a.time,
+            carModel: a.car,
+            request: "pending"
+        })
+        customer.save().then((updateRes) => res.send("Updated"))
+        Customerid.findByIdAndUpdate({ _id: "custId" }).then((db) => {
+            db.sequence_value = ++id
+            db.save()
+        })
     })
-    customer.save().then((updateRes) => res.send("Updated"))
 })
 
-app.post("/checkdealer",(req,res)=>{
+app.post("/checkdealer", (req, res) => {
     let a = req.body
     // console.log(a);
-    Dealername.find({ dealerEmail: a.email, dealerPass: a.pass }).then((db)=>{
-        if(Object.keys(db).length==0){
+    Dealername.find({ dealerEmail: a.email, dealerPass: a.pass }).then((db) => {
+        if (Object.keys(db).length == 0) {
             res.send("not found")
         }
-        else{
+        else {
             res.send("found")
         }
     })
 })
+app.post("/getdealerdeatils", (req, res) => {
+    let a = req.body
+    Dealername.find({ dealerEmail: a.Email }).then((db) => {
+        if (Object.keys(db).length == 0) {
+            res.send("not found")
+        }
+        else {
+            res.send(db)
+        }
+    })
+})
+
+app.post("/updatestatus", (req, res) => {
+    let a = req.body
+    if (a.status === "accept") {
+        Customerrequest.updateOne({ custId: { $eq: a.custId } }, { request: "accepted" }).then((db) => {
+            if (Object.keys(db).length == 0) {
+                res.send("not found")
+            }
+            else {
+                res.send("Updated")
+            }
+        })
+    }
+    else{
+        Customerrequest.updateOne({ custId: { $eq: a.custId } }, { request: "reject" }).then((db) => {
+            if (Object.keys(db).length == 0) {
+                res.send("not found")
+            }
+            else {
+                res.send("Updated")
+            }
+        })
+    }
+
+})
+
+
 
 
 app.listen(PORT)
